d3873b99982514c54c08ad8dda58f3431848b962
Your Name <vandathd49@gmail.com>
2022-05-03 20:07:43 +0700

    rewrite middleware done

--- BE/src/routes/postRouter.js
+++ BE/src/routes/postRouter.js
@@ -4,9 +4,9 @@ import postController from "../MC/Controller/postController.js";
 import upload from "../multer/multer.js";
 const postRouter = Router()
 postRouter.post('/create', upload.any(), middlewareController.verifyToken, postController.createPost)
-postRouter.patch('/update/:postId', upload.any(), middlewareController.verifyToken, middlewareController.verifyPost, postController.updatePost)
-postRouter.patch('/react/:postId', middlewareController.verifyToken, postController.reactPost)
-postRouter.delete('/delete/:postId', middlewareController.verifyToken, middlewareController.verifyPost, postController.deletePost)
+postRouter.patch('/update/:postId', upload.any(), middlewareController.verifyToken, middlewareController.verifyPost, middlewareController.isOwmerPost, postController.updatePost)
+postRouter.patch('/react/:postId', middlewareController.verifyToken, middlewareController.verifyPost, postController.reactPost)
+postRouter.delete('/delete/:postId', middlewareController.verifyToken, middlewareController.verifyPost, middlewareController.isOwmerPost, postController.deletePost)
 postRouter.get('/get/:postId', postController.getPost)
 postRouter.get('/getpostuser/:userId', postController.getPostUser)
 postRouter.get('/getall', postController.getAllPost)
