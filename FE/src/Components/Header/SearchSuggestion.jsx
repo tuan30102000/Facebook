08176625d0142063556f40112f1514d780084bab
Your Name <vandathd49@gmail.com>
2022-08-31 13:31:04 +0700

    fb

--- FE/src/Components/Header/SearchSuggestion.jsx
+++ FE/src/Components/Header/SearchSuggestion.jsx
@@ -5,13 +5,15 @@ SearchSuggetion.propTypes = {
     value: PropTypes.string,
     setvalue: PropTypes.func.isRequired,
 };
-function SearchSuggetion({ suggestList, indexTagret }) {
-
-
+function SearchSuggetion({ suggestList, indexTarget, value }) {
     return (
-        <div className='absolute'>
-            {suggestList.map((item, index) => <p key={item} className={clsx({ 'text-red-100': index == indexTagret })} >{item}</p>)
-            }
+        <div className='absolute top-full left-0 px-4 shadow bg-white w-full'>
+            {!value && <div className="flex">
+                <p className='font-[700] h-8 leading-8' >Lịch sử tìm kiếm</p>
+            </div>}
+            <ul>
+                {suggestList.map((item, index) => <p onClick={(e) => e.stopPropagation()} key={item} className={clsx('hover:bg-slate-50 h-10 leading-10 cursor-pointer', { 'text-red-500': index == indexTarget })} >{item}</p>)}
+            </ul>
         </div>
     );
 }
--- FE/src/hook/useThrotle.jsx
+++ FE/src/hook/useThrotle.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react"
-let timeOutId
+import { useRef, useState } from "react"
 const useInput = (fn, delay) => {
     let delayTime = delay || 0
     const [last, setlast] = useState(0)
-    const [timeOutId, settimeOutId] = useState()
+    const timeOutId = useRef(null)
     return () => {
-        if(timeOutId) {
-            clearTimeout(timeOutId)
+        if (timeOutId.current) {
+            clearTimeout(timeOutId.current)
         }
-        settimeOutId(setTimeout(fn,delay))
-        // timeOutId
+        timeOutId.current = setTimeout(() => {
+            console.log(22222)
+            fn()
+        }, delay)
         const dateNow = new Date().getTime()
         if (dateNow - last < delayTime) return
+        clearTimeout(timeOutId.current)
         fn()
         setlast(dateNow)
 
